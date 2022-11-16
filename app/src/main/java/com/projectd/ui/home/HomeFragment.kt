e2f6bbbd56982783fddaa7b12902bf813c4d0290
anggapamb <email_anda>
2022-11-16 16:54:52 +0700

    add data dummy home

--- app/src/main/java/com/projectd/ui/home/HomeFragment.kt
+++ app/src/main/java/com/projectd/ui/home/HomeFragment.kt
@@ -5,14 +5,200 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.content.res.AppCompatResources
+import androidx.core.app.ActivityCompat
+import androidx.core.view.isVisible
+import com.crocodic.core.base.adapter.CoreListAdapter
+import com.crocodic.core.helper.DateTimeHelper
 import com.projectd.R
 import com.projectd.base.fragment.BaseFragment
+import com.projectd.data.Cons
+import com.projectd.data.model.*
 import com.projectd.databinding.FragmentHomeBinding
+import com.projectd.databinding.ItemMainMenuBinding
+import com.projectd.databinding.ItemSecondMenuBinding
+import com.projectd.databinding.ItemUpdateBinding
 
 class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home) {
+
+    private val menus = ArrayList<HomeMenu?>()
+    private val addsMenus = ArrayList<AdditionalMenu?>()
+    private val listTask = ArrayList<Task?>()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        initView()
+        observe()
+        dataDummy()
+    }
+
+    private fun initView() {
+        binding?.tvDateNow?.text = DateTimeHelper().datePrettyNow()
+
+        binding?.rvMenu?.adapter = CoreListAdapter<ItemMainMenuBinding, HomeMenu>(R.layout.item_main_menu)
+            .initItem(menus) { position, data ->
+//                when (data?.key) {
+//                    "task" -> navigateTo(R.id.actionTaskFragment)
+//                    "project" -> navigateTo(R.id.actionProjectFragment)
+//                    "export_task" -> viewModel.exportTodayTask()
+//                }
+            }
+
+        binding?.rvMenuAdditional?.adapter = CoreListAdapter<ItemSecondMenuBinding, AdditionalMenu>(R.layout.item_second_menu)
+            .initItem(addsMenus) { position, data ->
+//                when (data?.key) {
+//                    "today_check" -> navigateTo(R.id.actionTodayCheckFragment)
+//                    "absent" -> AbsentDialog().show(childFragmentManager, "absent")
+//                    /*"overtime" -> requireActivity().openUrl(Cons.URL.OVERTIME)
+//                    "leave" -> requireActivity().openUrl(Cons.URL.LEAVE)
+//                    "reimbursement" -> requireActivity().openUrl(Cons.URL.REIMBURSEMENT)*/
+//                    else -> requireActivity().openUrl(data?.link ?: return@initItem)
+//                }
+            }
+    }
+
+    private fun observe() {
+        listTask.let { tasks ->
+            binding?.rvUpdate?.adapter = CoreListAdapter<ItemUpdateBinding, Task>(R.layout.item_update).initItem(tasks)
+        }
+    }
+
+    private fun dataDummy() {
+        /* user */
+        val user = User(
+            photo = "https://thumbs.dreamstime.com/b/beige-cat-placard-his-neck-says-bad-cat-prison-black-lineup-background-cat-placard-prison-167328390.jpg",
+            name = "Angga Pambudi",
+            username = "anggapamb",
+            specialAccess = listOf("rtask")
+        )
+        binding?.user = user
+
+        /* prayer */
+        val prayer = Prayer(
+            image = "https://c-fa.cdn.smule.com/rs-s-sg-1/sing_google/performance/cover/db/76/244786ea-1547-4341-95f2-3f0951ac2598.jpg",
+            title = "Pak Haji Jamaludin",
+            description = "Bismillah"
+        )
+        binding?.prayer = prayer
+
+        /* homeMenu */
+        val homeMenu = arrayOf(
+            HomeMenu(
+                icon = R.drawable.ic_baseline_inbox_24,
+                color = R.color.text_bg_blue,
+                background = AppCompatResources.getDrawable(requireContext(), R.drawable.bg_round_12_blue),
+                label = "Inbox",
+                key = "inbox",
+                count = 0,
+                countBackground = R.drawable.bg_circle_blue
+            ),
+            HomeMenu(
+                icon = R.drawable.ic_baseline_playlist_add_check_24,
+                color = R.color.text_bg_green,
+                background = AppCompatResources.getDrawable(requireContext(), R.drawable.bg_round_12_green),
+                label = "Today\'s Task",
+                key = "task",
+                count = 0,
+                countBackground = R.drawable.bg_circle_green
+            ),
+            HomeMenu(
+                icon = R.drawable.ic_baseline_architecture_24,
+                color = R.color.text_bg_yellow,
+                background = AppCompatResources.getDrawable(requireContext(), R.drawable.bg_round_12_yellow),
+                label = "Project",
+                key = "project",
+                count = 0,
+                countBackground = R.drawable.bg_circle_yellow
+            )
+        )
+        menus.addAll(homeMenu)
+
+        /* menu */
+        val addMenus = arrayOf(
+            AdditionalMenu(
+                image = "https://static.thenounproject.com/png/1049455-200.png",
+                label = "Today Check",
+                key = "today_check"
+            ), AdditionalMenu(
+                image = "https://static.thenounproject.com/png/1049455-200.png",
+                label = "Today Check",
+                key = "today_check"
+            ), AdditionalMenu(
+                image = "https://static.thenounproject.com/png/1049455-200.png",
+                label = "Today Check",
+                key = "today_check"
+            ), AdditionalMenu(
+                image = "https://static.thenounproject.com/png/1049455-200.png",
+                label = "Today Check",
+                key = "today_check"
+            ), AdditionalMenu(
+                image = "https://static.thenounproject.com/png/1049455-200.png",
+                label = "Today Check",
+                key = "today_check"
+            ),
+        )
+        addsMenus.addAll(addMenus)
+        binding?.vMenuAdditional?.isVisible = addsMenus.isNotEmpty()
+
+        /* absent */
+        val absent = Absent(description = "Ijin Work From Home")
+        binding?.absent = absent
+
+        /* task */
+        val task = arrayOf(
+            Task(
+                engineer = Task.User(
+                    photo = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ0y1PpEz2R27LVDEXmvaFag3DqVevjSHW_0w&usqp=CAU",
+                    name = "Uzumaki Naruto",
+                    username = "naruto",
+                    role = "Engineer"),
+                createdBy = "You",
+                description = "Membuat jalan",
+                project = Task.Project(name = "Pengecoran Jalan Anyer - Panarukan"),
+                notes = "Membangun jalan raya dari Anyer sampai Panarukan"
+            ),Task(
+                engineer = Task.User(
+                    photo = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ0y1PpEz2R27LVDEXmvaFag3DqVevjSHW_0w&usqp=CAU",
+                    name = "Uzumaki Naruto",
+                    username = "naruto",
+                    role = "Engineer"),
+                createdBy = "You",
+                description = "Membuat jalan",
+                project = Task.Project(name = "Pengecoran Jalan Anyer - Panarukan"),
+                notes = "Membangun jalan raya dari Anyer sampai Panarukan"
+            ),Task(
+                engineer = Task.User(
+                    photo = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ0y1PpEz2R27LVDEXmvaFag3DqVevjSHW_0w&usqp=CAU",
+                    name = "Uzumaki Naruto",
+                    username = "naruto",
+                    role = "Engineer"),
+                createdBy = "You",
+                description = "Membuat jalan",
+                project = Task.Project(name = "Pengecoran Jalan Anyer - Panarukan"),
+                notes = "Membangun jalan raya dari Anyer sampai Panarukan"
+            ),Task(
+                engineer = Task.User(
+                    photo = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ0y1PpEz2R27LVDEXmvaFag3DqVevjSHW_0w&usqp=CAU",
+                    name = "Uzumaki Naruto",
+                    username = "naruto",
+                    role = "Engineer"),
+                createdBy = "You",
+                description = "Membuat jalan",
+                project = Task.Project(name = "Pengecoran Jalan Anyer - Panarukan"),
+                notes = "Membangun jalan raya dari Anyer sampai Panarukan"
+            ),Task(
+                engineer = Task.User(
+                    photo = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ0y1PpEz2R27LVDEXmvaFag3DqVevjSHW_0w&usqp=CAU",
+                    name = "Uzumaki Naruto",
+                    username = "naruto",
+                    role = "Engineer"),
+                createdBy = "You",
+                description = "Membuat jalan",
+                project = Task.Project(name = "Pengecoran Jalan Anyer - Panarukan"),
+                notes = "Membangun jalan raya dari Anyer sampai Panarukan"
+            ),
+        )
+        listTask.addAll(task)
     }
 }

bb08866fcee8c52e3ce40396ad10acc061348437
anggapamb <email_anda>
2023-03-29 14:14:45 +0700

    feat:force logout

--- app/src/main/java/com/projectd/base/observe/BaseObserve.kt
+++ app/src/main/java/com/projectd/base/observe/BaseObserve.kt
@@ -1,6 +1,5 @@
 package com.projectd.base.observe
 
-import com.crocodic.core.api.ApiCode
 import com.crocodic.core.api.ApiObserver
 import com.crocodic.core.api.ApiResponse
 import com.crocodic.core.data.CoreSession
@@ -8,12 +7,18 @@ import com.projectd.api.ApiService
 import com.projectd.data.Cons
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.asSharedFlow
 import org.json.JSONObject
 
 class BaseObserver(
     private val apiService: ApiService,
     private val session: CoreSession,
 ) {
+
+    private val _sendResponseStatus = MutableSharedFlow<String>()
+    val responseStatus = _sendResponseStatus.asSharedFlow()
+
     operator fun invoke(
         block: suspend () -> String,
         dispatcher: CoroutineDispatcher = Dispatchers.IO,
@@ -29,21 +34,26 @@ class BaseObserver(
             }
 
             override suspend fun onError(response: ApiResponse) {
-                val status = response.rawResponse?.let { JSONObject(it) }?.getInt("code")
-                if (status == ApiCode.EXPIRED) {
-                    ApiObserver(
-                        { apiService.refreshToken() },
-                        responseListener = object : ApiObserver.ResponseListener {
-                            override suspend fun onSuccess(response: JSONObject) {
-                                val token = response.getString("access_token")
-                                session.setValue(Cons.DB.USER.ACCESS_TOKEN, token)
-                                ApiObserver(block, responseListener = responseListener)
-                            }
+                val message = response.rawResponse?.let { JSONObject(it) }?.getString("message")
+                if (message == "Unauthorized!") {
+                    val error = response.rawResponse?.let { JSONObject(it) }?.getJSONObject("err")?.getString("name")
+                    if (error == "TokenNotFoundInDB") {
+                        _sendResponseStatus.emit("logout")
+                    } else {
+                        ApiObserver(
+                            { apiService.refreshToken() },
+                            responseListener = object : ApiObserver.ResponseListener {
+                                override suspend fun onSuccess(response: JSONObject) {
+                                    val token = response.getString("access_token")
+                                    session.setValue(Cons.DB.USER.ACCESS_TOKEN, token)
+                                    ApiObserver(block, responseListener = responseListener)
+                                }
 
-                            override suspend fun onError(response: ApiResponse) {
-                                responseListener.onError(response)
-                            }
-                        })
+                                override suspend fun onError(response: ApiResponse) {
+                                    responseListener.onError(response)
+                                }
+                            })
+                    }
                 }
                 responseListener.onError(response)
             }
--- app/src/main/java/com/projectd/ui/home/HomeActivity.kt
+++ app/src/main/java/com/projectd/ui/home/HomeActivity.kt
@@ -1,6 +1,7 @@
 package com.projectd.ui.home
 
 import android.Manifest
+import android.app.AlertDialog
 import android.content.ComponentName
 import android.content.Intent
 import android.content.pm.PackageManager
@@ -8,6 +9,9 @@ import android.os.Build
 import android.os.Bundle
 import androidx.activity.OnBackPressedCallback
 import androidx.annotation.RequiresApi
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import com.crocodic.core.data.CoreSession
@@ -17,14 +21,23 @@ import com.permissionx.guolindev.PermissionX
 import com.projectd.R
 import com.projectd.base.App
 import com.projectd.base.activity.BaseActivity
+import com.projectd.base.observe.BaseObserver
+import com.projectd.data.Session
 import com.projectd.data.model.Prayer
 import com.projectd.databinding.ActivityHomeBinding
 import com.projectd.service.fcm.FirebaseMsgService
 import com.projectd.service.prayer.PrayerPlayer
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import org.greenrobot.eventbus.Subscribe
 import org.greenrobot.eventbus.ThreadMode
+import org.koin.android.ext.android.inject
+import timber.log.Timber
 
 class HomeActivity : BaseActivity<ActivityHomeBinding, HomeViewModel>(R.layout.activity_home) {
+
+    private val observer: BaseObserver by inject()
+    private val session: Session by inject()
 
     private val navHostFragment: NavHostFragment by lazy { supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment }
     private val navController: NavController by lazy { navHostFragment.navController }
@@ -33,11 +46,14 @@ class HomeActivity : BaseActivity<ActivityHomeBinding, HomeViewModel>(R.layout.a
 
     private var backPressed: Long = 0
 
+    private lateinit var alert : AlertDialog
+
     @RequiresApi(Build.VERSION_CODES.TIRAMISU)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         clearNotification()
 
+        alertDialogForceLogout()
         binding.vPrayerInformation.setOnClickListener { navController.navigate(R.id.actionHomeFragment) }
         //initDailySetup()
 
@@ -46,19 +62,63 @@ class HomeActivity : BaseActivity<ActivityHomeBinding, HomeViewModel>(R.layout.a
         permissionNotification()
 
         onBackPressedHandle()
+
+
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                launch {
+                    observer.responseStatus.collect {
+                        if (it == "logout") {
+                            if (!alert.isShowing) {
+                                alertDialogForceLogout(true)
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private fun alertDialogForceLogout(isShow: Boolean = false) {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Akses Ditolak!")
+        builder.setMessage("Anda login di perangkat lain, anda akan diarahkan ke halaman Login.")
+        builder.setPositiveButton("OK") { _, _ ->
+            session.clearAll()
+            navController.navigate(R.id.actionLoginFragment)
+        }
+        alert = builder.create()
+        alert.setCancelable(false)
+        if (isShow) alert.show()
     }
 
     private fun fixedNotification() {
         val intent = Intent()
-        when(Build.MANUFACTURER) {
-            "xiaomi" -> intent.component= ComponentName("com.miui.securitycenter", "com.miui.permcenter.autostart.AutoStartManagementActivity")
-            "oppo" -> intent.component = ComponentName("com.coloros.safecenter", "com.coloros.safecenter.permission.startup.StartupAppListActivity")
-            "vivo" -> intent.component = ComponentName("com.vivo.permissionmanager", "com.vivo.permissionmanager.activity.BgStartUpManagerActivity")
-            "Letv" -> intent.component = ComponentName("com.letv.android.letvsafe", "com.letv.android.letvsafe.AutobootManageActivity")
-            "Honor" -> intent.component = ComponentName("com.huawei.systemmanager", "com.huawei.systemmanager.optimize.process.ProtectActivity")
+        when (Build.MANUFACTURER) {
+            "xiaomi" -> intent.component = ComponentName(
+                "com.miui.securitycenter",
+                "com.miui.permcenter.autostart.AutoStartManagementActivity"
+            )
+            "oppo" -> intent.component = ComponentName(
+                "com.coloros.safecenter",
+                "com.coloros.safecenter.permission.startup.StartupAppListActivity"
+            )
+            "vivo" -> intent.component = ComponentName(
+                "com.vivo.permissionmanager",
+                "com.vivo.permissionmanager.activity.BgStartUpManagerActivity"
+            )
+            "Letv" -> intent.component = ComponentName(
+                "com.letv.android.letvsafe",
+                "com.letv.android.letvsafe.AutobootManageActivity"
+            )
+            "Honor" -> intent.component = ComponentName(
+                "com.huawei.systemmanager",
+                "com.huawei.systemmanager.optimize.process.ProtectActivity"
+            )
         }
 
-        val list = this.packageManager.queryIntentActivities(intent, PackageManager.MATCH_DEFAULT_ONLY)
+        val list =
+            this.packageManager.queryIntentActivities(intent, PackageManager.MATCH_DEFAULT_ONLY)
         if (list.size > 0) {
             this.startActivity(intent)
         }
@@ -69,10 +129,20 @@ class HomeActivity : BaseActivity<ActivityHomeBinding, HomeViewModel>(R.layout.a
         PermissionX.init(this)
             .permissions(Manifest.permission.POST_NOTIFICATIONS)
             .onExplainRequestReason { scope, deniedList ->
-                scope.showRequestReasonDialog(deniedList, "Core fundamental are based on these permissions", "OK", "Cancel")
+                scope.showRequestReasonDialog(
+                    deniedList,
+                    "Core fundamental are based on these permissions",
+                    "OK",
+                    "Cancel"
+                )
             }
             .onForwardToSettings { scope, deniedList ->
-                scope.showForwardToSettingsDialog(deniedList, "You need to allow necessary permissions in Settings manually", "OK", "Cancel")
+                scope.showForwardToSettingsDialog(
+                    deniedList,
+                    "You need to allow necessary permissions in Settings manually",
+                    "OK",
+                    "Cancel"
+                )
             }
             .request { allGranted, _, _ ->
                 if (!allGranted) {
@@ -82,7 +152,10 @@ class HomeActivity : BaseActivity<ActivityHomeBinding, HomeViewModel>(R.layout.a
     }
 
     private fun fromNotificationUpdateTask() {
-        CoreSession(this).setValue(IS_UPDATE_TASK, intent.getBooleanExtra(FirebaseMsgService.NOTIFICATION_UPDATE_TASK, false))
+        CoreSession(this).setValue(
+            IS_UPDATE_TASK,
+            intent.getBooleanExtra(FirebaseMsgService.NOTIFICATION_UPDATE_TASK, false)
+        )
     }
 
     /* worker prayer
@@ -106,7 +179,8 @@ class HomeActivity : BaseActivity<ActivityHomeBinding, HomeViewModel>(R.layout.a
     }
 
     fun invalidateTickerPlayer() {
-        binding.prayer = if (navController.currentDestination?.id != R.id.homeFragment && isPlayPrayer()) prayerPlayer.prayer else null
+        binding.prayer =
+            if (navController.currentDestination?.id != R.id.homeFragment && isPlayPrayer()) prayerPlayer.prayer else null
     }
 
     @Subscribe(sticky = true, threadMode = ThreadMode.MAIN)
